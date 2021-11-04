f2a230e21ec10ae2cbf7d8253ae62d010a4fa72b
root <ark10806@gmail.com>
2021-11-04 14:18:33 +0000

    final_bunch

--- pre/EDA/rgbd.py
+++ pre/EDA/rgbd.py
@@ -8,28 +8,28 @@ import json
 
 dataroot = '/opt/ml/segmentation/input/data'
 
-# all_paths = glob(os.path.join(dataroot, '**', '*.jpg'))
-# print(len(all_paths))
+all_paths = glob(os.path.join(dataroot, '**', '*.jpg'))
+print(len(all_paths))
 
-# r = []
-# g = []
-# b = []
+r = []
+g = []
+b = []
 
-# for path in tqdm(all_paths):
-#     img = np.array(Image.open(path))
-#     # print(img.shape)
-#     r.append(img[:,:,0])
-#     g.append(img[:,:,1])
-#     b.append(img[:,:,2])
+for path in tqdm(all_paths):
+    img = np.array(Image.open(path))
+    # print(img.shape)
+    r.append(img[:,:,0])
+    g.append(img[:,:,1])
+    b.append(img[:,:,2])
 
-# print(len(r), len(g), len(b))
-# print(np.mean(r))
-# print(np.mean(g))
-# print(np.mean(b))
+print(len(r), len(g), len(b))
+print(np.mean(r))
+print(np.mean(g))
+print(np.mean(b))
 
-# print(np.std(r))
-# print(np.std(g))
-# print(np.std(b))
+print(np.std(r))
+print(np.std(g))
+print(np.std(b))
 
-trains = json.load(open('/opt/ml/segmentation/input/data/train.json', 'r'))['images']
-print(len(trains))
+# trains = json.load(open('/opt/ml/segmentation/input/data/train.json', 'r'))['images']
+# print(len(trains))
